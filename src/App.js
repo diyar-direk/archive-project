2f8d03ad862736fefe9e3c2c85a140d1b7013565
diyar-direk <diyardireki111@gmail.com>
2024-12-04 02:20:33 -0800

    s

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import AddPerson from './pages/pepole/AddPerson';
+import AddPerson from "./pages/pepole/AddPerson";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="*" element={<Dashboard />}>
-          <Route path="add_person" element={<AddPerson/>} />
+          <Route path="add_person" element={<AddPerson />} />
         </Route>
       </Routes>
     </div>
