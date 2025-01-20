c181c495e5c63249f96175e1219e697812b47823
Diyar-Direki <diyardireki111@gmail.com>
2025-01-20 21:37:52 +0300

    info page

--- src/pages/info/InfoPage.jsx
+++ src/pages/info/InfoPage.jsx
@@ -37,45 +37,45 @@ const InfoPage = () => {
   ) : (
     <div className="single-info">
       <h1> {data.subject} </h1>
-      <h2>details:</h2>
+      <h2>details</h2>
       <p>{data.details}</p>
-      <h2>note:</h2>
+      <h2>note</h2>
       <p>{data.note}</p>
       <div className="flex align-center gap-10">
-        <h3> country:</h3>
+        <h2> country</h2>
         <p>{data.countryId?.name}</p>
       </div>
       <div className="flex align-center gap-10">
-        <h3> city:</h3>
+        <h2> city</h2>
         <p>{data.cityId?.name}</p>
       </div>
       <div className="flex align-center gap-10">
-        <h3> government:</h3>
+        <h2> government</h2>
         <p>{data.governmentId?.name}</p>
       </div>
       <div className="flex align-center gap-10">
-        <h3>region:</h3>
+        <h2>region</h2>
         <p>{data.regionId ? data.regionId?.name : "no region found"}</p>
       </div>
       <div className="flex align-center gap-10">
-        <h3>street:</h3>
+        <h2>street</h2>
         <p>{data.streetId ? data.streetId?.name : "no street found"}</p>
       </div>
       <div className="flex align-center gap-10">
-        <h3>village:</h3>
+        <h2>village</h2>
         <p>{data.villageId ? data.villageId?.name : "no village found"}</p>
       </div>
       <div className="flex align-center gap-10">
-        <h3>addressDetails:</h3>
+        <h2>addressDetails</h2>
         <p>{data.addressDetails ? data.addressDetails : "no Details found"}</p>
       </div>
 
+      <CategoriesShow
+        title="coordinates"
+        data={data.coordinates}
+        name="coordinates"
+      />
       <div className="categories grid-3">
-        <CategoriesShow
-          title="coordinates"
-          data={data.coordinates}
-          name="coordinates"
-        />
         <CategoriesShow title="people" name="people" data={data.people} />
         <CategoriesShow title="events" data={data.events} name="name" />
         <CategoriesShow title="parties" data={data.parties} name="name" />
