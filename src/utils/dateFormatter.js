471820cf005429f0efeb9ebc1489f7bba9e5f922
diyar-direk <diyardireki111@gmail.com>
2025-08-21 02:31:29 -0700

    info

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -11,9 +11,10 @@ export const dateFormatter = (date, format = "justYear") => {
   if (format === "justYear") return `${year}-${month}-${day}`;
   const hoursFormat =
     time.getHours() > 12 ? time.getHours() - 12 : time.getHours();
-  const at = time.getHours() > 12 ? "PM" : "AM";
+  const at = time.getHours() >= 12 ? "PM" : "AM";
   const hours = String(hoursFormat).padStart(2, "0");
   const minutes = String(time.getMinutes()).padStart(2, "0");
   const seconds = String(time.getSeconds()).padStart(2, "0");
+
   return `${year}-${month}-${day} / ${hours}:${minutes}:${seconds} ${at}`;
 };
